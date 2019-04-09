8d9e35cee8990fb1c087ad8fe8d701576e873f84
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-09 12:32:25 -0700

    Guessing game draft 1

---
+++ game.py
@@ -0,0 +1,35 @@
+"""A number-guessing game."""
+
+# Put your code here
+
+# greet player
+# get player name
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
+
+def playGame():
+    print("Welcome to the guessing game!")
+    name = input("What is your name?")
+    import random
+    number =  random.randrange(1, 101)
+    guesscount = 0
+    #print (number)
+    while True:
+        useranswer = int(input("{}, choose a number betwee 1 and 100\n".format(name)))
+        guesscount += 1
+        #print(answer)
+        if number != useranswer:
+            if number > useranswer:
+                print("Your number, {}, is too small".format(useranswer))
+            else:
+                print("Your number, {}, is too large".format(useranswer))
+        else:
+            return("Well done, {}! {} is the right answer!! You found my number in {} tries.".format(name, useranswer, guesscount))
+            #break
+print(playGame())
